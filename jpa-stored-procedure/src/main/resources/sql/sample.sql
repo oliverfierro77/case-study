f9f993f03156d4eaf365b7379fa9e62ab003f71b
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-08-11 16:23:53 +0300

    also add the script for the new procedure

--- jpa-stored-procedure/src/main/resources/sql/sample.sql
+++ jpa-stored-procedure/src/main/resources/sql/sample.sql
@@ -19,5 +19,23 @@ CREATE OR REPLACE PROCEDURE FILTER_EMPLOYEES(name IN VARCHAR2, ref_cursor OUT SY
   END FILTER_EMPLOYEES;
 /
 
+CREATE OR REPLACE PROCEDURE search_employees(
+    in_last_name  IN VARCHAR2,
+    in_first_name IN VARCHAR2,
+    ref_cursor OUT SYS_REFCURSOR)
+AS
+BEGIN
+  OPEN ref_cursor
+  FOR
+    SELECT id,
+    last_name,
+    first_name,
+    birth_date
+    FROM employee
+    WHERE last_name LIKE '%' || NVL(in_first_name, '') || '%'
+    OR first_name LIKE '%' || NVL(in_last_name, '') || '%';
+END search_employees;
+/
+
 INSERT INTO employee VALUES (1, '19-JAN-1990', 'Ilon', 'Paiva', 'M');
 INSERT INTO employee VALUES (2, '12-MAR-92', 'Scub', 'Slub', 'F')
