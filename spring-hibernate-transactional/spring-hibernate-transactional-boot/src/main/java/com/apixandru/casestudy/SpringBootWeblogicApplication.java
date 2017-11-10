8325307debca6ccd33c957490795e74d55c05420
Alexandru-Constantin Bledea <contact@apixandru.com>
2017-11-10 09:10:10 +0200

    upgrade spring boot to version 2.x

--- spring-hibernate-transactional/spring-hibernate-transactional-boot/src/main/java/com/apixandru/casestudy/SpringBootWeblogicApplication.java
+++ spring-hibernate-transactional/spring-hibernate-transactional-boot/src/main/java/com/apixandru/casestudy/SpringBootWeblogicApplication.java
@@ -1,18 +1,25 @@
 package com.apixandru.casestudy;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringBootWeblogicApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+public class SpringBootWeblogicApplication
+        extends SpringBootServletInitializer
+        implements WebApplicationInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SpringBootWeblogicApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWeblogicApplication.class, args);
     }
 
 }
